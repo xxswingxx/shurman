b0808978f3498150b2185765a37876a2c352d155
John Wu <vidadelaempresa@gmail.com>
2014-04-06 10:04:33 +0100

    Test worker

--- app/workers/pinger.rb
+++ app/workers/pinger.rb
@@ -7,6 +7,6 @@ class Pinger
   def perform
     response = HTTParty.get 'http://www.forocoches.com/foro/register.php'
     match = response.parsed_response.encode('utf-8').match(/INVITACIÓN REQUERIDA PARA NUEVOS REGISTROS/)
-    UserMailer.notify_good_news.deliver if match.nil?
+    UserMailer.notify_good_news.deliver
   end
 end
